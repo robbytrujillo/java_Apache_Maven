9d831b8adb3cbab506ea71a0670c817f6bb72d68
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-08-24 08:52:47 +0700

    Java Apache Maven

---
+++ src/main/java/robby/code/maven/App.java
@@ -0,0 +1,13 @@
+package robby.code.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class git initApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
